ebb9532acb4606a789f8fb4bd6ca9114eea93f40
HyeongYoon <baseball2173@gmail.com>
2025-02-25 16:27:09 +0900

    first commit

---
+++ fitzCode/src/main/java/kr/co/fitzcode/FitzCodeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fitzcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitzCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitzCodeApplication.class, args);
+    }
+
+}
