47a15eaf394572c30dc5fbf6b166c8e26b107111
HyeongYoon <baseball2173@gmail.com>
2025-02-26 21:06:55 +0900

    관리자 공지사항 CRUD GlobalExceptionHandler errorPage.html

--- fitzCode/src/main/java/kr/co/fitzcode/common/exception/GlobalExceptionHandler.java
+++ fitzCode/src/main/java/kr/co/fitzcode/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package kr.co.fitzcode.common.exception;
 
 import kr.co.fitzcode.admin.exception.NoticeNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,19 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoticeNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNoticeNotFoundException(NoticeNotFoundException e, Model model) {
         model.addAttribute("error", e.getMessage());
+        log.error("Notice를 찾을 수 없음: {}", e.getMessage(), e);
         return "errorPage";
     }
 
     @ExceptionHandler(Exception.class)
     public String handleGeneralException(Exception e, Model model) {
         model.addAttribute("error", "예상치 못한 오류가 발생했습니다.");
-        e.printStackTrace();
+        log.error("예상치 못한 오류 발생: {}", e.getMessage(), e);
         return "errorPage";
     }
 }
