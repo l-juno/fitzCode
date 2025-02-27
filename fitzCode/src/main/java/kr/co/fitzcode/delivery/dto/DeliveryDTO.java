382a1cf62e31dd9570003923ac6b599fc845e463
Ritto <ritto7875@gmail.com>
2025-02-27 14:07:47 +0900

    택배 api 시험

---
+++ fitzCode/src/main/java/kr/co/fitzcode/delivery/dto/DeliveryDTO.java
@@ -0,0 +1,10 @@
+package kr.co.fitzcode.delivery.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeliveryDTO {
+}
---
+++ fitzCode/src/main/java/kr/co/fitzcode/delivery/service/DeliveryService.java
@@ -0,0 +1,7 @@
+package kr.co.fitzcode.delivery.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeliveryService {
+}
