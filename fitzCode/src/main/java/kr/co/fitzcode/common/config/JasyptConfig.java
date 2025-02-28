b1cc5d7e5c5162ff09e57b206ef79f27355d05bd
HyeongYoon <baseball2173@gmail.com>
2025-02-28 10:16:07 +0900

    [add] TEST jasypt

---
+++ fitzCode/src/main/java/kr/co/fitzcode/common/config/JasyptConfig.java
@@ -0,0 +1,35 @@
+package kr.co.fitzcode.common.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        String encryptorKey = System.getenv("ENCRYPTOR_KEY");
+//        System.out.println(encryptorKey);
+        if (encryptorKey == null || encryptorKey.isEmpty()) {
+            throw new IllegalStateException("환경변수 ENCRYPTOR_KEY가 설정되지 않았습니다.");
+        }
+
+        config.setPassword(encryptorKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
