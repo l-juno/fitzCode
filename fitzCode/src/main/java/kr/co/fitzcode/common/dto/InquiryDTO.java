02b400314ed9b8fbe50dff9b7fc5a857d06e1f6f
Ritto <ritto7875@gmail.com>
2025-03-10 20:51:35 +0900

    inquiry 수정 완

--- fitzCode/src/main/java/kr/co/fitzcode/common/dto/InquiryDTO.java
+++ fitzCode/src/main/java/kr/co/fitzcode/common/dto/InquiryDTO.java
@@ -2,9 +2,7 @@ package kr.co.fitzcode.common.dto;
 
 import kr.co.fitzcode.common.enums.InquiryCategory;
 import kr.co.fitzcode.common.enums.InquiryStatus;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -12,7 +10,7 @@ import java.util.List;
 
 @Getter
 @Setter
-@Builder
+@NoArgsConstructor
 public class InquiryDTO {
     private int inquiryId;           // 문의 ID
     private int userId;              // 문의한 사용자 ID
--- fitzCode/src/main/java/kr/co/fitzcode/inquiry/control/InquiryController.java
+++ fitzCode/src/main/java/kr/co/fitzcode/inquiry/control/InquiryController.java
@@ -4,6 +4,7 @@ package kr.co.fitzcode.inquiry.control;
 import jakarta.servlet.http.HttpSession;
 import kr.co.fitzcode.common.dto.InquiryDTO;
 import kr.co.fitzcode.common.dto.UserDTO;
+import kr.co.fitzcode.common.util.SecurityUtils;
 import kr.co.fitzcode.inquiry.service.InquiryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,12 +21,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InquiryController {
     private final InquiryService inquiryService;
+    private final SecurityUtils securityUtils;
 
     // 문의 메뉴로 이동
     @GetMapping("/inquiryForm")
-    public String inquiryForm(Model model, HttpSession session) {
-        session.setAttribute("userId", 20);
-        int userId = (int) session.getAttribute("userId");
+    public String inquiryForm(Model model) {
+        int userId = securityUtils.getUserId();
         UserDTO dto = inquiryService.getUserOne(userId);
         model.addAttribute("userDTO", dto);
         return "inquiry/inquiryForm";
@@ -40,11 +41,9 @@ public class InquiryController {
     }
 
     // 개인 문의 내역 보기
-    // 나중에 세션 생기면 @SessionAttribute("userId") int userId 사용하기
     @GetMapping("/inquiryList")
-    public String inquiryList(Model model, HttpSession session) {
-        session.setAttribute("userId", 20);
-        int userId = (int) session.getAttribute("userId");
+    public String inquiryList(Model model) {
+        int userId = securityUtils.getUserId();
         List<HashMap<String, Object>> list = inquiryService.getInquiryList(userId);
         model.addAttribute("list", list);
         return "inquiry/inquiryList";
