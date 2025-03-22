35abd04bd7904bec28ba32ba43b20723e24fd2cf
HyeongYoon <baseball2173@gmail.com>
2025-03-22 15:45:04 +0900

    [fix] 쿠폰자동발급

--- fitzCode/src/main/java/kr/co/fitzcode/common/mapper/UserCouponMapper.java
+++ fitzCode/src/main/java/kr/co/fitzcode/common/mapper/UserCouponMapper.java
@@ -1,4 +1,10 @@
 package kr.co.fitzcode.common.mapper;
 
-public class UserCouponMapper {
+import kr.co.fitzcode.common.dto.UserCouponDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserCouponMapper {
+    void assignCouponToUser(UserCouponDTO userCoupon);
+    int countUserCoupon(UserCouponDTO userCoupon);
 }
