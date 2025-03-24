a3011c3bbad569aad25c5b88995a97742002498e
doyxxn <doyeon0099@gmail.com>
2025-03-24 17:43:53 +0900

    [commit] 페이지 추가

---
+++ fitzCode/src/main/java/kr/co/fitzcode/common/dto/PostLikeDTO.java
@@ -0,0 +1,18 @@
+package kr.co.fitzcode.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostLikeDTO {
+    private int likeId;         // 좋아요 고유 ID
+    private int postId;         // 게시물 ID
+    private int userId;         // 사용자 ID
+}
---
+++ fitzCode/src/main/java/kr/co/fitzcode/community/controller/CommentController.java
@@ -0,0 +1,4 @@
+package kr.co.fitzcode.community.controller;
+
+public class CommentController {
+}
